b41516bcdff6395995dab718e913f70914d1b172
Superduper-India <89244209+Superduper-India@users.noreply.github.com>
2022-07-21 20:49:09 +0900

    [level2] 정렬 - 가장 큰 수 문제풀이

--- programmers/level1/biggest-number/biggest-number.test.js
+++ programmers/level1/biggest-number/biggest-number.test.js
@@ -1,13 +1,14 @@
 function solution(numbers) {
-  const stringNumber = numbers.map(number => number.toString());
-  return stringNumber.sort((a, b) => (b + a) - (a + b)).join("");
+  const toString = numbers.map(number => { return String(number); });
+  const answer = toString.sort((a, b) => (b + a) - (a + b)).join("");
+
+  return answer[0] === "0" ? "0" : answer;
 }
 
 test("run", () => {
   expect(solution(
     [6, 10, 2]
   )).toEqual("6210");
-
   expect(solution(
     [3, 30, 34, 5, 9]
   )).toEqual("9534330");
