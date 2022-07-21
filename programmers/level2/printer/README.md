4f1e2644b4d958da3f9515bc50cd199855c27ad8
Superduper-India <89244209+Superduper-India@users.noreply.github.com>
2022-07-21 13:11:34 +0900

    [level2] 스택/큐 - 프린터 문제풀이

--- programmers/level2/printer/README.md
+++ programmers/level2/printer/README.md
@@ -5,25 +5,18 @@
 현재 대기목록에 있는 문서의 중요도가 순서대로 담긴 배열 priorities와 내가 인쇄를 요청한 문서가 현재 대기목록의 어떤 위치에 있는지를 알려주는 location이 매개변수로 주어질 때, 내가 인쇄를 요청한 문서가 몇 번째로 인쇄되는지 return 하도록 solution 함수를 작성
 
 ### 조건
-1. 인쇄 대기목록의 가장 앞에 있는 문서(J)를 대기목록에서 꺼냅니다.
-<br />
-2. 나머지 인쇄 대기목록에서 J보다 중요도가 높은 문서가 한 개라도 존재하면 J를 대기목록의 가장 마지막에 넣습니다.
-<br />
-3. 그렇지 않으면 J를 인쇄합니다.
-<br />
-4. 인쇄 작업의 중요도는 1~9로 표현하며 **숫자가 클수록** 중요하다는 뜻입니다.
-<br />
-5. location은 0 이상 (현재 대기목록에 있는 작업 수 - 1) 이하의 값을 가지며 대기목록의 가장 앞에 있으면 0, 두 번째에 있으면 1로 표현합니다.
+* 인쇄 대기목록의 가장 앞에 있는 문서(J)를 대기목록에서 꺼냅니다.
+* 나머지 인쇄 대기목록에서 J보다 중요도가 높은 문서가 한 개라도 존재하면 J를 대기목록의 가장 마지막에 넣습니다.
+* 그렇지 않으면 J를 인쇄합니다.
+* 인쇄 작업의 중요도는 1~9로 표현하며 **숫자가 클수록** 중요하다는 뜻입니다.
+* location은 0 이상 (현재 대기목록에 있는 작업 수 - 1) 이하의 값을 가지며 대기목록의 가장 앞에 있으면 0, 두 번째에 있으면 1로 표현합니다.
 
 ## 2. 계획
-실행 가능한 계획
-그리고 문제에서 하라는 대로
+1/ priorities 의 initialLocation을 기록해야한다. 빈 객체인 initialLocation를 파고, priorities를 맵으로 순회해서 initialLocation[index] = priority 로 설정해준다.
 
+2/ priorities를 맵으로 순회하여 priority가 Math.max(...priorities)와 같지 않다면, priorities.shift(priority) => push해준다.
 
-
-1/ priorities[location] 값보다 큰값이 priorities에 있으면 priorities[location].splice(location, length-1)로써 가장 마지막 인덱스로 옮기기
-2/ priorities[location]이 가장 크면 answer배열에 push
-3/ answer.indexOf(priorities[location]) 구하기
+/ initialLocation의 key값이 location에 해당되는 것의 최종 index+1위치를 구하면 되는 것이다~~
 
 ## 3. 실행
 ***
--- programmers/level2/printer/printer.test.js
+++ programmers/level2/printer/printer.test.js
@@ -1,71 +1,26 @@
-// 1. 문제를 코드로 번역하기
-// 2. 새로운 사실을 알면 계획을 수정하기
-// 3. 문맥 헷갈리지 말기
-// 4. Host와 Service 분리해서 생각해보기
+function solution(priorities, location) {
+  // 처음 위치를 기록해놓은 프린트 우선순위 
+  // 배열 [0]이 처음순서고, [1]이 중요도
+  const initialPriorities = priorities.map((priority, index) =>
+    [index, priority]
+  );
+  const donePrint = [];
+  priorities.sort((a, b) => b - a);
 
-const isExistMoreThan = (value, arr) => {
-  for (const v of arr) {
-    if (value.priority < v.priority) {
-      return true;
+  while (priorities.length) {
+    if (initialPriorities[0][1] == priorities[0]) {
+      donePrint.push(initialPriorities.shift()[0]);
+      priorities.shift();
+    } else {
+      const tmp = initialPriorities.shift();
+      initialPriorities.push(tmp);
     }
   }
 
-  return false;
-};
-
-// host
-// service
-const print = (priorities) => {
-  // 1. 인쇄 대기목록의 가장 앞에 있는 문서(J)를 대기목록에서 꺼냅니다.
-  const result = priorities.shift();
-  // 2. 나머지 인쇄 대기목록에서 J보다 중요도가 높은 문서가 한 개라도 존재하면 J를 대기목록의 가장 마지막에 넣습니다.
-  if (isExistMoreThan(result, priorities)) {
-    priorities.push(result);
-    return [null, priorities];
-  }
-
-  // 3. 그렇지 않으면 J를 인쇄합니다.
-  return [result, priorities];
-};
-
-const run = (priorities, location, count = 0) => {
-  // 프린트해라
-  const [result, newPriorities] = print(priorities);
-  if (!result) {
-    return run(newPriorities, location, count);
-  }
-
-  if (result.originIndex !== location) {
-    return run(newPriorities, location, count + 1);
-  }
-
-  return count + 1;
-};
-function solution(priorities, location) {
-  const p = priorities.map((priority, index) => ({
-    originIndex: index,
-    priority,
-  }));
-
-  return run(p, location);
-
-
-  // const print = [];
-
-  // priorities.map(number => {
-  //   if (priorities[location] < number) {
-  //     priorities.splice(priorities[location], 1).push(priorities[location]);
-  //   } else if (priorities[location] > number) {
-  //     print.push(priorities[location]);
-  //   }
-  // });
-
+  return donePrint.indexOf(location) + 1; // location인덱스가 donePrint배열중 어디에 위치하는지 찾아줌.
 }
 
 test("run", () => {
-  expect(solution([2, 1, 3, 2], 2)).toBe(1);
-  expect(solution([2, 1, 3, 2], 1)).toBe(4);
-  expect(solution([2, 1, 3, 2], 0)).toBe(3);
-  expect(solution([2, 1, 3, 2], 3)).toBe(2);
-  expect(solution([1, 1, 9, 1, 1, 1], 0)).toBe(5);
+  expect(solution([2, 1, 3, 2], 2)).toEqual(1);
+  expect(solution([1, 1, 9, 1, 1, 1], 0)).toEqual(5);
 });
