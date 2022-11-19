d193c79e27433ab55a03f400540a18ee505adc40
Superduper-India <89244209+Superduper-India@users.noreply.github.com>
2022-11-19 16:12:25 +0900

    [level1] 콜라츠 추측 재귀 문제풀이

--- programmers/level1/Collatz-guess/Collatz-guess.test.js
+++ programmers/level1/Collatz-guess/Collatz-guess.test.js
@@ -1,26 +1,34 @@
-function solution(num) {
-  let count = 0;
+const isEven = (number) => number % 2 === 0;
 
+function solution(num, count = 0) {
   // 재귀
-  // if (num !== 1) { // num이 1이 아닌경우에 반복
-  //   if (num % 2 === 1) { // 홀수
-  //     solution((num * 3) + 1);
-  //   } else { // 짝수
-  //     solution(num / 2);
-  //   }
-  // } return num;
+  if (count >= 500) {
+    return -1;
+  }
 
   if (num === 1) {
-    return 0;
-  } else {
-    while (num !== 1) { // num이 1이 될때까지 반복문 시행
-      count++;
-      num % 2 ? num = (num * 3) + 1 : num = num / 2;
-      if (count === 500) break;
-    }
-    if (count === 500) return -1;
     return count;
   }
+
+  if (isEven(num)) {
+    return solution(num / 2, count + 1);
+  } else {
+    return solution(num * 3 + 1, count + 1);
+  }
+
+
+  // 반복문
+  // if (num === 1) {
+  //   return 0;
+  // } else {
+  //   while (num !== 1) { // num이 1이 될때까지 반복문 시행
+  //     count++;
+  //     num % 2 ? num = (num * 3) + 1 : num = num / 2;
+  //     if (count === 500) break;
+  //   }
+  //   if (count === 500) return -1;
+  //   return count;
+  // }
 }
 
 test("run", () => {
