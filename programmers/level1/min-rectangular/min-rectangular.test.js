087eae09a7399057d266e0b724f704936f1f36db
Superduper-India <89244209+Superduper-India@users.noreply.github.com>
2022-11-19 20:50:33 +0900

    [level1] 최소직사각형 문제풀이

--- programmers/level1/min-rectangular/min-rectangular.test.js
+++ programmers/level1/min-rectangular/min-rectangular.test.js
@@ -1,28 +1,39 @@
 function solution(sizes) {
-  const sortSizes = sizes.map(size => {
-    if (size[0] < size[1]) {
-      let temp = size[0];
-      size[0] = size[1];
-      size[1] = temp;
-      return size;
-    } else {
-      return size;
-    }
+  const wallet = sizes.map(card => {
+    return card[0] < card[1] ? [card[1], card[0]] : [card[0], card[1]];
   });
 
-  const maxW = [];
-  const maxH = [];
-  sortSizes.forEach(size => {
-    const [w, h] = size;
-    maxW.push(w);
-    maxH.push(h);
-  });
+  const width = wallet.map(card => { return card[0]; });
+  const height = wallet.map(card => { return card[1]; });
 
-  return Math.max(...maxW) * Math.max(...maxH);
+  return Math.max(...width) * Math.max(...height);
 }
 
-test("solution", () => {
-  expect(solution([[60, 50], [30, 70], [60, 30], [80, 40]])).toBe(4000);
-  expect(solution([[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]])).toBe(120);
-  expect(solution([[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]])).toBe(133);
+test("run", () => {
+  expect(solution(
+    [
+      [60, 50],
+      [30, 70],
+      [60, 30],
+      [80, 40]
+    ]
+  )).toBe(4000);
+  expect(solution(
+    [
+      [10, 7],
+      [12, 3],
+      [8, 15],
+      [14, 7],
+      [5, 15]
+    ]
+  )).toBe(120);
+  expect(solution(
+    [
+      [14, 4],
+      [19, 6],
+      [6, 16],
+      [18, 7],
+      [7, 11]
+    ]
+  )).toBe(133);
 });
