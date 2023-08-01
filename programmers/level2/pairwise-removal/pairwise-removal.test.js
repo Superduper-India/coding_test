3a645cd5f074e843a2885b145ea7bb0030869d0d
Superduper-India <sunyoung1542@gmail.com>
2023-08-01 10:11:38 +0900

    [level2] 짝지어 제거하기 문제풀이

--- programmers/level2/pairwise-removal/pairwise-removal.test.js
+++ programmers/level2/pairwise-removal/pairwise-removal.test.js
@@ -1,6 +1,17 @@
-const solution = (s) => {};
+const solution = (s) => {
+  let stringArr = [...s];
 
-test('run', () => {
-  expect(solution('baabaa')).toBe(1);
-  expect(solution('cdcd')).toBe(0);
+  for (let i = 0; i < stringArr.length - 1; i++) {
+    // 지금 인덱스와 다음 인덱스의 문자가 일치하면 stringArr에서 해당 인덱스들을 제거한다.
+    if (stringArr[i] === stringArr[i + 1]) {
+      stringArr.splice(i, 2);
+    }
+  }
+
+  return stringArr.length === 2 ? 1 : 0;
+};
+
+test("run", () => {
+  expect(solution("baabaa")).toBe(1);
+  expect(solution("cdcd")).toBe(0);
 });
