4c04e7efbb167418b83d3a20fd4041bc174631e9
Superduper-India <sunyoung1542@gmail.com>
2023-08-05 17:10:54 +0900

    [level2] 귤 고르기 문제 다시 풀기

--- programmers/level2/pick-a-tangerine/pick-a-tangerine.test.js
+++ programmers/level2/pick-a-tangerine/pick-a-tangerine.test.js
@@ -1,23 +1,16 @@
 const solution = (k, tangerine) => {
-  // tangerine배열에서 각요소의 중복개수를 각각 구한다.
-  const obj = tangerine.reduce((acc, curr) => ({ ...acc, [curr]: (acc[curr] || 0) + 1 }), {});
+  let map = new Map();
+  tangerine.forEach((dd) => {
+    map.set(dd, (map.get(dd) || 0) + 1);
+  });
 
-  const count = Object.values(obj).sort((a, b) => b - a);
+  const maxNum = Math.max(...map.values());
 
-  let sum = 0;
-  let answer = 0;
-  // count를 하나씩 체크한다. 정렬해서 가장 큰 수부터 체크된다.
-  for (let num of count) {
-    // 한번 for문이 돌 때 마다 answer값에 1을 증가시키고,
-    // sum값에 들어온 값들을 더해준다.
-    answer++;
-    sum += num;
-
-    // sum의 값이 k보다 크거나 같으면 최솟값을 충족했으므로 for문을 중지시킨다.
-    if (sum >= k) break;
+  if (maxNum > k) {
+    return 1;
+  } else {
+    return k / maxNum;
   }
-
-  return answer;
 };
 
 test('run', () => {
