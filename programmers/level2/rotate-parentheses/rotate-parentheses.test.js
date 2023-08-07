0fd4a540fd27cd686cb5262d22a937ace4a7bc72
Superduper-India <sunyoung1542@gmail.com>
2023-08-07 14:42:10 +0900

    [level2] 괄호 회전하기 문제 다시 풀기

--- programmers/level2/rotate-parentheses/rotate-parentheses.test.js
+++ programmers/level2/rotate-parentheses/rotate-parentheses.test.js
@@ -1,39 +1,35 @@
-// 문자열 첫글자를 문자열 마지막으로 이동하여 반환
-const makeRotation = (string) => {
-  return string.substring(1, string.length) + string[0];
+const checkTheCorrection = (string) => {
+  const tempArr = [];
+  let result;
+
+  for (let i = 0; i < string.length; i += 2) {
+    tempArr.push([string[i], string[i + 1]]);
+  }
+
+  tempArr.forEach(([left, right]) => {
+    if (
+      (left === "[" && right === "]") ||
+      (left === "(" && right === ")") ||
+      (left === "{" && right === "}")
+    )
+      result = true;
+    else result = false;
+  });
+
+  return result;
+};
+
+const rotation = (string) => {
+  return string.substring(1) + string[0];
 };
 
 const solution = (s) => {
   let count = 0;
 
-  // 올바른 문자열인지 확인하는 함수
-  const checkTheCorrect = (string) => {
-    const arr = [];
-    let result;
-
-    for (let i = 0; i < string.length; i += 2) {
-      arr.push([string[i], string[i + 1]]);
-    }
-
-    arr.forEach((item) => {
-      const left = item[0];
-      const right = item[1];
-      if (
-        (left === "[" && right === "]") ||
-        (left === "(" && right === ")") ||
-        (left === "{" && right === "}")
-      ) {
-        result = true;
-      } else result = false;
-    });
-
-    return result;
-  };
-
+  // 0부터 5까지 반복문으로 돌리기
   for (let i = 0; i < s.length; i++) {
-    const result = checkTheCorrect(s);
-    s = makeRotation(s);
-    if (result) count += 1;
+    s = rotation(s);
+    console.log(checkTheCorrection(s));
   }
 
   return count;
