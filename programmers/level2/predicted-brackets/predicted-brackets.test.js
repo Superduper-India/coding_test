714756371a543d38559e20d62665c90c01ec504a
Superduper-India <sunyoung1542@gmail.com>
2023-08-09 09:49:35 +0900

    [level2] 예상 대진표 문제 다시 풀기

--- programmers/level2/predicted-brackets/predicted-brackets.test.js
+++ programmers/level2/predicted-brackets/predicted-brackets.test.js
@@ -1,13 +1,18 @@
 const solution = (n, a, b) => {
-  let count = 0;
+  let round = 0;
 
-  while (a !== b) {
-    a = Math.ceil(a / 2);
-    b = Math.ceil(b / 2);
-    count += 1;
-  }
+  // 재귀함수로 문제를 풀었다.
+  const self = (a, b) => {
+    console.log(a, b);
+    // 기저조건. a와 b가 같아지면 재귀함수 호출을 멈춘다.
+    if (a === b) return;
+    // a와 b가 같지 않으면 다음 경기를 진행한다. (각 참가자는 n/2번의 번호를 부여받는다.)
+    round++;
+    self(Math.ceil(a / 2), Math.ceil(b / 2));
+  };
+  self(a, b);
 
-  return count;
+  return round;
 };
 
 test("run", () => {
