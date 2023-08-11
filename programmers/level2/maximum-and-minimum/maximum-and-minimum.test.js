1088e16a01e54c90a1aa94b6c163458df60f90e9
Superduper-India <sunyoung1542@gmail.com>
2023-08-11 10:46:43 +0900

    [level2] 최댓값과 최솟값 문제 다시 풀기

--- programmers/level2/maximum-and-minimum/maximum-and-minimum.test.js
+++ programmers/level2/maximum-and-minimum/maximum-and-minimum.test.js
@@ -1,39 +1,8 @@
-// 이전 숫자와 현재 숫자가 있을때 더 작은 숫자를 찾는 로직
-const makeMin = (numberArr) => {
-  let min = 0;
-  // 비교당할 숫자
-  const target = numberArr[0];
-  for (let j = 1; j < numberArr.length; j++) {
-    // 비교할 현재 숫자
-    const now = numberArr[j];
-    if (target < now) min = target;
-    else min = now;
-  }
-  return min;
-};
-
-// 이전 숫자와 현재 숫자가 있을때 더 큰 숫자를 찾는 로직
-const makeMax = (numberArr) => {
-  let max = 0;
-  // 비교당할 숫자
-  const target = numberArr[0];
-  for (let j = 1; j < numberArr.length; j++) {
-    // 비교할 현재 숫자
-    const now = numberArr[j];
-    if (target > now) max = target;
-    else max = now;
-  }
-  return max;
-};
-
 const solution = (s) => {
-  const numberArr = s.split(" ").map((number) => +number);
-  // console.log(">>>numberArr", numberArr);
+  const stringArr = s.split(" ");
 
-  const min = makeMin(numberArr);
-  const max = makeMax(numberArr);
-
-  return min + " " + max;
+  // Math.max, Math.min함수가 문자열도 취급한다.
+  return Math.min(...stringArr) + " " + Math.max(...stringArr);
 };
 
 test("run", () => {
