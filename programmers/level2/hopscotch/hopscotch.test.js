4e67f735779d5d2579a0813edf9ac94242b3117d
Superduper-India <sunyoung1542@gmail.com>
2023-08-15 17:09:37 +0900

    [level2] 땅따먹기 문제 다시 풀기

--- programmers/level2/hopscotch/hopscotch.test.js
+++ programmers/level2/hopscotch/hopscotch.test.js
@@ -1,24 +1,16 @@
 function solution(land) {
-  let memory = [];
-
-  // 아래 풀이도 시간초과 ㅠㅠ
-  for (let i = 0; i < land.length; i++) {
-    if (i === 0) {
-      memory = land[i];
-    } else {
-      let maxI = memory.indexOf(Math.max(...memory));
-      let temp = memory.indexOf(Math.max(...memory));
-      memory = land[i];
-      for (let j = 0; j < memory.length; j++) {
-        if (j === maxI) {
-          temp = j - 1;
-          memory[j] = memory[j] + land[i - 1][temp];
-        } else memory[j] = memory[j] + land[i - 1][maxI];
-      }
+  // 1. 0번 인덱스 이후의 1, 2번 인덱스부터 시작한다.
+  for (let i = 1; i < land.length; i++) {
+    // 2. 열의 4개의 요소를 반복문으로 탐색한다.
+    for (let j = 0; j < 4; j++) {
+      // 3. 이전 열에서 현재 인덱스를 제외한 요소들 중 최댓값 구하기
+      const preMaxNum = Math.max(...land[i - 1].filter((_, preI) => j !== preI));
+      // 4. 현재 열의 요소들에 각각 위의 최댓값을 더해준다.
+      land[i][j] = land[i][j] + preMaxNum;
     }
   }
 
-  return Math.max(...memory);
+  return Math.max(...land.pop());
 }
 
 test('run', () => {
