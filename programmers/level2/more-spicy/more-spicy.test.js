89d6da96697527744ed132cc4543d1dc220743a9
Superduper-India <sunyoung1542@gmail.com>
2023-08-17 21:09:23 +0900

    [level2] 더 맵게 문제 다시 풀기

--- programmers/level2/more-spicy/more-spicy.test.js
+++ programmers/level2/more-spicy/more-spicy.test.js
@@ -1,21 +1,21 @@
 // 음식의 스코빌 지수를 담은 배열 scoville, 원하는 스코빌 지수 K
 const solution = (scoville, K) => {
   let answer = 0;
-  let temp = [...scoville];
 
-  // 실패 => 시간초과
-  while (true) {
-    const min = temp.shift();
-    const secMin = temp.shift();
-    // scovile에서 가장 작은 수가 K이상이면 반복문 탈출
-    if (min >= K) break;
-    if (temp.length == 1 && temp.shift() < K) {
+  // 실패 => 런타임 에러
+  while (scoville[0] < K) {
+    // 가장 작은 숫자
+    const minN = Math.min(...scoville);
+    // 그 다음 가장 작은 숫자
+    const secMinN = Math.min(...scoville.filter((_, i) => i !== scoville.indexOf(minN)));
+
+    scoville[0] = minN + secMinN * 2;
+    scoville.splice(scoville.indexOf(secMinN), 1);
+    answer++;
+    if (scoville.length === 1) {
       answer = -1;
       break;
     }
-    // 위에 해당하지 않는다면, 스코빌 지수가 낮은 순서대로 섞어주기
-    answer++;
-    temp = [min + secMin * 2, ...temp];
   }
 
   return answer;
