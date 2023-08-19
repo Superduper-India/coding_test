306ac71ef2c3f7b30e824a4314adb407c993dcd0
Superduper-India <sunyoung1542@gmail.com>
2023-08-19 15:31:03 +0900

    [level2] 피보나치 수 문제 다시 풀기

--- programmers/level2/fibonacci-sequence/fibonacci-sequence.test.js
+++ programmers/level2/fibonacci-sequence/fibonacci-sequence.test.js
@@ -1,8 +1,9 @@
 const solution = (n) => {
   let record = [0, 1];
 
+  // 테스트 케이스 7부터 실패 => 여기에서 % 1234567 연산자를 적용해주면 통과
   for (let i = 2; i <= n; i++) {
-    record[i] = record[i - 1] + record[i - 2];
+    record[i] = (record[i - 1] + record[i - 2]) % 1234567;
   }
 
   return record[n];
