f117507c53d3d387844607e8d929515688a3850a
Superduper-India <sunyoung1542@gmail.com>
2023-08-29 14:01:57 +0900

    [level2] k진수에서 소수 개수 구하기 문제 풀기

--- programmers/level2/prime-number-from-binary/prime-number-from-binary.test.js
+++ programmers/level2/prime-number-from-binary/prime-number-from-binary.test.js
@@ -8,6 +8,7 @@ makeBiany = (targetNumber, k) => {
 };
 
 // 소수인지 판별하는 함수
+// toDo 제곱근까지만 나눠보기
 const isPrime = (num) => {
   //1은 소수가 아니다.
   if (num === 1) return false;
@@ -21,19 +22,50 @@ const isPrime = (num) => {
 };
 
 const solution = (n, k) => {
+  let answer = 0;
+
   // 1. 10진법을 k진법으로 변환하는 함수를 호출한다.
   const biany = makeBiany(n, k);
 
   // 2. 숫자들중에 0을 기준으로 나눈 배열을 만든다.
-  const temp = biany.split("0").map((num) => +num);
-  // 3. 배열을 순회해서 각 요소가 소수인지 판별한다.
-  temp.forEach((number, index) => {
-    if (isPrime(number)) {
-      console.log("소수다", index, String(number).length);
-    } else console.log("소수가 아니다", index);
-    // 4. 요소가 소수이면 조건 1~4까지를 판별한다.
-    // 그러려면 ['211', '0', '2', '0', '1', '0', '1', '0', '11']이렇게 해서 위치를 구해야할듯
+  const temp = biany.split("0");
+
+  const arr = [];
+  let string = "";
+  biany.split("").forEach((number, index) => {
+    if (+number > 0) {
+      string = string + number;
+    } else {
+      arr.push(string);
+      string = number;
+      arr.push(string);
+      string = "";
+    }
+
+    if (biany.split("").length - 1 === index) {
+      arr.push(string);
+    }
   });
+
+  // 3. 배열을 순회해서 각 요소가 소수인지 판별한다.
+  arr.forEach((number, index) => {
+    if (number != 0) {
+      if (isPrime(+number)) {
+        // console.log("소수다", number);
+        // 4. 요소가 소수이면 조건 1~4까지를 판별한다.
+        // (1) 0P0처럼 소수 양쪽에 0이 있는 경우
+        if (arr[index - 1] == 0 && arr[index + 1] == 0) answer++;
+        // (2) P0처럼 소수 오른쪽에만 0이 있고 왼쪽에는 아무것도 없는 경우
+        else if (arr[index - 1] == undefined && arr[index + 1] == 0) answer++;
+        // (3) 0P처럼 소수 왼쪽에만 0이 있고 오른쪽에는 아무것도 없는 경우
+        else if (arr[index + 1] == undefined && arr[index - 1] == 0) answer++;
+        // (4) P처럼 소수 양쪽에 아무것도 없는 경우
+      }
+      // else console.log("소수가 아니다", number);
+    }
+  });
+
+  return answer;
 };
 
 test("run", () => {
