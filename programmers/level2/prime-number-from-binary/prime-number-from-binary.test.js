68cc77c116e9115d4489b2ff4322667f975e83a9
Superduper-India <sunyoung1542@gmail.com>
2023-08-29 19:43:36 +0900

    [level2] k진수에서 소수 개수 구하기 문제 다시 풀기

--- programmers/level2/prime-number-from-binary/prime-number-from-binary.test.js
+++ programmers/level2/prime-number-from-binary/prime-number-from-binary.test.js
@@ -1,19 +1,36 @@
-// k진수를 10진수로 변환해주는 함수
-makeDecimal = (targetNumber, k) => {
-  return parseInt(targetNumber, k);
-};
 // 10진수를 k진수로 변환해주는 함수
-makeBiany = (targetNumber, k) => {
+const makeBiany = (targetNumber, k) => {
   return targetNumber.toString(k);
 };
 
+// 타겟이 될 배열을 반환해주는 함수
+const makeTargetArr = (biany) => {
+  const arr = [];
+  let string = '';
+  biany.split('').forEach((number, index) => {
+    if (+number > 0) {
+      string = string + number;
+    } else {
+      arr.push(string);
+      string = number;
+      arr.push(string);
+      string = '';
+    }
+
+    if (biany.split('').length - 1 === index) {
+      arr.push(string);
+    }
+  });
+
+  return arr;
+};
+
 // 소수인지 판별하는 함수
-// toDo 제곱근까지만 나눠보기
 const isPrime = (num) => {
-  //1은 소수가 아니다.
-  if (num === 1) return false;
+  // 0이나 1은 소수가 아니다.
+  if (!num || num === 1) return false;
   // 1과 자기자신을 제외하고 반복문을 수행하도록 한다.
-  for (let i = 2; i < num; i++) {
+  for (let i = 2; i <= Math.sqrt(num); i++) {
     //num이 다른 수로 나눠떨어진다면 소수가 아니다.
     if (num % i === 0) return false;
   }
@@ -26,49 +43,32 @@ const solution = (n, k) => {
 
   // 1. 10진법을 k진법으로 변환하는 함수를 호출한다.
   const biany = makeBiany(n, k);
+  const arr = makeTargetArr(biany);
 
-  // 2. 숫자들중에 0을 기준으로 나눈 배열을 만든다.
-  const temp = biany.split("0");
+  console.log(arr);
 
-  const arr = [];
-  let string = "";
-  biany.split("").forEach((number, index) => {
-    if (+number > 0) {
-      string = string + number;
-    } else {
-      arr.push(string);
-      string = number;
-      arr.push(string);
-      string = "";
-    }
-
-    if (biany.split("").length - 1 === index) {
-      arr.push(string);
-    }
-  });
-
-  // 3. 배열을 순회해서 각 요소가 소수인지 판별한다.
+  // 2. 배열을 순회해서 각 요소가 소수인지 판별한다.
   arr.forEach((number, index) => {
-    if (number != 0) {
-      if (isPrime(+number)) {
-        // console.log("소수다", number);
-        // 4. 요소가 소수이면 조건 1~4까지를 판별한다.
-        // (1) 0P0처럼 소수 양쪽에 0이 있는 경우
-        if (arr[index - 1] == 0 && arr[index + 1] == 0) answer++;
-        // (2) P0처럼 소수 오른쪽에만 0이 있고 왼쪽에는 아무것도 없는 경우
-        else if (arr[index - 1] == undefined && arr[index + 1] == 0) answer++;
-        // (3) 0P처럼 소수 왼쪽에만 0이 있고 오른쪽에는 아무것도 없는 경우
-        else if (arr[index + 1] == undefined && arr[index - 1] == 0) answer++;
-        // (4) P처럼 소수 양쪽에 아무것도 없는 경우
-      }
-      // else console.log("소수가 아니다", number);
+    if (isPrime(+number)) {
+      // console.log("소수다", number);
+      // 3. 요소가 소수이면 조건 1~4까지를 판별한다.
+      // (1) 0P0처럼 소수 양쪽에 0이 있는 경우
+      if (arr[index - 1] == 0 && arr[index + 1] == 0) answer++;
+      // (2) P0처럼 소수 오른쪽에만 0이 있고 왼쪽에는 아무것도 없는 경우
+      else if (arr[index - 1] == undefined && arr[index + 1] == 0) answer++;
+      // (3) 0P처럼 소수 왼쪽에만 0이 있고 오른쪽에는 아무것도 없는 경우
+      else if (arr[index + 1] == undefined && arr[index - 1] == 0) answer++;
+      // (4) P처럼 소수 양쪽에 아무것도 없는 경우
+      else if (arr[index + 1] == undefined && arr[index - 1] == undefined) answer++;
     }
+    // console.log("소수가 아니다", number);
   });
 
   return answer;
 };
 
-test("run", () => {
+test('run', () => {
   expect(solution(437674, 3)).toBe(3);
   expect(solution(110011, 10)).toBe(2);
+  expect(solution(885733, 3)).toBe(1);
 });
