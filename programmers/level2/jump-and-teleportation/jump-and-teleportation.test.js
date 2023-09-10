4d35ca641775fecbe90c58b21736a92451c3cfe0
Superduper-India <sunyoung1542@gmail.com>
2023-09-11 07:32:03 +0900

    [level2] 점프와 순간 이동 문제 풀기

--- programmers/level2/jump-and-teleportation/jump-and-teleportation.test.js
+++ programmers/level2/jump-and-teleportation/jump-and-teleportation.test.js
@@ -4,54 +4,24 @@
 // 조건
 // 아이언 슈트: 1. 한 번에 K칸을 앞으로 점프(K만큼 건전지 사용량이 줄어듬), 2. 현재까지 온 거리 x2에 해당하는 위치로 순간이동(건전지 사용량이 줄지 않음)
 
+// 즉 순간이동을 최대한 해야한다. n에서 도착지점으로 돌아온다고 가정했을때 2로 계속해서 나누어준다. 나누어 떨어지면 n만 2로 나눈값을 재할당해주고 계속 가지만, 나누어 떨어지지 않는다면 한칸만 가는 것이므로 n을 하나만 빼주고, answer(건전지 사용량)을 하나만 증가시켜준다.
+
 const solution = (n) => {
-  // 남은 거리는 n이다.
-  let distance = n;
-  // 현재까지 온 거리는 전체 거리에서 남은거리를 뺀 거리이다.
-  let current = 0;
-  // 건전지 사용량은 점프함수에서만 추가된다.
-  let usedBateri = 0;
-  const answer = [];
+  let answer = 0;
 
-  // 점프하는 함수 distance~1까지 인수가 주어진다.
-  const jump = (jumpDistance) => {
-    // 남은 거리를 업데이트 한다.
-    distance = distance - jumpDistance;
-    // 현재까지 온 거리를 업데이트 한다.
-    current = n - distance;
-    // 사용한 건전지 사용량을 업데이트 한다.
-    usedBateri += jumpDistance;
-  };
-
-  // 순간이동하는 함수
-  const getTeleportation = () => {
-    // 남은 거리를 업데이트 한다.
-    distance = n - current * 2;
-    current = n - distance;
-  };
-
-  console.log(n % 2);
-
-  // jump(5);
-
-  // jump(2);
-  // getTeleportation();
-  // jump(1);
-
-  // jump(1);
-  // getTeleportation();
-  // getTeleportation();
-  // jump(1);
-
-  for (let i = n; i > 0; i--) {
-    if (i % 2 == 1) {
-      // i가 홀수인 경우
+  // n이 0보다 큰 경우에만 지속한다.
+  while (n > 0) {
+    if (n % 2 === 0) {
+      n /= 2;
+      // console.log('짝수인 경우', n);
     } else {
-      // i가 짝수인 경우
+      n--;
+      // console.log('홀수인 경우', n);
+      answer++;
     }
   }
 
-  console.log('남은거리', distance, '현재까지 온 거리', current, '사용한 배터리', usedBateri, answer);
+  return answer;
 };
 
 test('run', () => {
