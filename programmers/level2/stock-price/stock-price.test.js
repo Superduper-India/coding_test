7f055ef10a8702b05aec68dd7da02f16792d8db8
Superduper-India <sunyoung1542@gmail.com>
2023-09-20 07:17:02 +0900

    [level2] 주식 가격 문제 다시 풀기

--- programmers/level2/stock-price/stock-price.test.js
+++ programmers/level2/stock-price/stock-price.test.js
@@ -16,16 +16,39 @@ const solution = (prices) => {
   // [5] 3 => ? : ?, prices.length - 5
 
   for (let i = 0; i < len; i++) {
-    const tempNum = i + 1;
-    if (prices[i + 1] > prices[i]) answer.push(len - tempNum);
-    else if (prices[i + 1] < prices[i]) answer.push(prices[i] - prices[i + 1]);
-    else answer.push(0);
+    let stack = prices[i]; // 현재 주식 가격
+    let j = i + 1; // 비교 할 수의 index
+    let sec = 0; // 가격이 떨어지지 않은 시간
+
+    while (j < len + 1) {
+      // j < 6 이면 진행
+      if (stack <= prices[j]) {
+        // 1. 나의 주식 가격이 다음 주식 가격보다 작거나 같으면 통과.
+        sec += 1;
+        j += 1;
+        continue; // if문 끝냄 while문으로
+      } else if (sec == 0 && stack > prices[j]) {
+        // 2. 나의 주식 가격이 다음 주식 가격보다 크면 종료.
+        sec = 1;
+        answer.push(sec);
+        sec = 0;
+        break; // while문 끝냄
+      } else if (sec == 0 && j == len) {
+        // 3. 마지막 주식이면 0 반환.
+        answer.push(sec);
+        break; // while문 끝냄
+      }
+      answer.push(sec);
+      sec = 0;
+      break;
+    }
   }
 
+  console.log(answer);
   return answer;
 };
 
 test('run', () => {
   expect(solution([1, 2, 3, 2, 3])).toStrictEqual([4, 3, 1, 1, 0]);
-  // expect(solution([5, 8, 6, 2, 4, 1])).toStrictEqual([3, 1, 1, 2, 1, 0]);
+  expect(solution([5, 8, 6, 2, 4, 1])).toStrictEqual([3, 1, 1, 2, 1, 0]);
 });
