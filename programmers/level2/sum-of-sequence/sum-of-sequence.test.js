849b5567037a673e37a98d1a0d5e67c0ae710e52
Superduper-India <sunyoung1542@gmail.com>
2023-10-11 22:42:58 +0900

    [level2] 연속된 부분 수열의 합 문제 풀기

---
+++ programmers/level2/sum-of-sequence/sum-of-sequence.test.js
@@ -0,0 +1,48 @@
+// 원하는 것
+//수열을 나타내는 정수 배열 sequence와 부분 수열의 합을 나타내는 정수 k가 매개변수로 주어질 때, 위 조건을 만족하는 부분 수열의 시작 인덱스와 마지막 인덱스를 배열에 담아 return 하는 solution 함수를 완성
+
+// 조건
+// 기존 수열에서 임의의 두 인덱스의 원소와 그 사이의 원소를 모두 포함하는 부분 수열
+// 부분 수열의 합은 k
+// 합이 k인 부분 수열이 여러 개인 경우 길이가 짧은 수열을 찾기
+// 길이가 짧은 수열이 여러 개인 경우 앞쪽(시작 인덱스가 작은)에 나오는 수열을 찾기
+
+const solution = (sequence, k) => {
+  let i = 0;
+  let temp = [];
+  let size = 0;
+  const answer = [0, sequence.length - 1];
+
+  while (i < sequence.length) {
+    const map = new Map();
+    let calc = 0;
+    // 부분 수열의 합이 k를 만족하는 경우의 수를 모두 찾는다.
+    for (let j = i; j < sequence.length; j++) {
+      map.set(j, sequence[j]);
+      calc = calc + sequence[j];
+
+      if (calc === k) {
+        answer[0] = [...map][0][0];
+        answer[1] = [...map][[...map].length - 1][0];
+      }
+    }
+    i++;
+  }
+
+  return answer;
+
+  // 이 중 길이가 가장 짧은 것을 찾는다.
+  // 길이가 동일하면 시작 인덱스가 작은 수열을 찾는다.
+  // const min = [];
+  // temp.forEach((map) => {
+  //   min.push(map.size);
+  // });
+  // const minNum = Math.min(...min);
+  // const dd = temp.filter((map) => map.size === minNum);
+};
+
+test('run', () => {
+  // expect(solution([1, 2, 3, 4, 5], 7)).toStrictEqual([2, 3]);
+  expect(solution([1, 1, 1, 2, 3, 4, 5], 5)).toStrictEqual([6, 6]);
+  expect(solution([2, 2, 2, 2, 2], 6)).toStrictEqual([0, 2]);
+});
