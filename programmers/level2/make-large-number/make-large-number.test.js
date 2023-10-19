c70b1763ec70b41c564932bf439a95904afbe7e7
Superduper-India <sunyoung1542@gmail.com>
2023-10-20 07:30:35 +0900

    [level2] 큰 수 만들기 문제 다시 풀기

--- programmers/level2/make-large-number/make-large-number.test.js
+++ programmers/level2/make-large-number/make-large-number.test.js
@@ -9,26 +9,26 @@
 // 해답 검사(Solution Check): 원래의 문제가 해결되었는지 검사하고, 해결되지 않았다면 선택 절차로 돌아가 위의 과정을 반복한다.
 
 const solution = (number, k) => {
-  // 0123
-  // 23 => 13 => 12 => 03 => 02 => 01
-
   const arr = [];
-
-  for (let i = k; i >= 0; i--) {
-    for (let j = number.length - 1; j >= 0; j--) {
-      console.log('i', i, 'j', j);
-      if (i === j) break;
-      else {
-        arr.push(+(number[i] + number[j]));
-      }
+  for (let i = 0; i < number.length; i++) {
+    // 배열 마지막 숫자와 현재 숫자를 비교하며 현재 숫자가 더 크면 해당 숫자를 pop함
+    while (arr.length && arr[arr.length - 1] < number[i] && k > 0) {
+      // 위 과정을 현재 숫자보다 큰 수가 나올때까지 최대 k번 반복함
+      k--;
+      arr.pop();
     }
+    arr.push(number[i]);
   }
+  // 모든 숫자를 비교한 후 k가 0보다 크면 남은 k만큼 뒤에서 제거함
+  // 배열의 끝에서 'k'개의 요소를 제거 => 마지막 테스트 케이스 고려
+  arr.splice(number.length - k, k);
 
-  return String(Math.max(...arr));
+  return arr.join('');
 };
 
 test('run', () => {
   expect(solution('1924', 2)).toBe('94');
   expect(solution('1231234', 3)).toBe('3234');
   expect(solution('4177252841', 4)).toBe('775841');
+  expect(solution('7777777', 2)).toBe('77777');
 });
