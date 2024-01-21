b2d0eebe996e80906cd65264836a72fc94431282
Superduper-India <sunyoung1542@gmail.com>
2024-01-21 19:12:00 +0900

    [level2] 조이스틱 문제 다시 풀기

--- programmers/level2/joy-stick/joy-stick.test.js
+++ programmers/level2/joy-stick/joy-stick.test.js
@@ -7,56 +7,54 @@
 // ◀ - 커서를 왼쪽으로 이동 (첫 번째 위치에서 왼쪽으로 이동하면 마지막 문자에 커서)
 // ▶ - 커서를 오른쪽으로 이동 (마지막 위치에서 오른쪽으로 이동하면 첫 번째 문자에 커서)
 
-// 3. 계획
-// (1) 조이스틱의 상,하 이동: A부터 오름차순으로 찾는게 빠른지, Z부터 내림차순으로 찾는게 빠른지 확인하기
-// (2) 조이스틱의 좌, 우 이동: 만들고자 하는 문자열이 모두 연속된 A로만 이루어져 있다면 오른쪽으로만 이동하는 것이 최소한의 움직임으로 문자열을 완성시킬 수 있다. 하지만, 그렇지 않을 경우에는 왼쪽으로 이동하는 것이 빠른 경우가 있다. 그래서 좌->우 이동과 우->좌 이동의 횟수를 비교해서 최솟값을 찾아야 한다.
-
-// 주어진 알파벳을 아스키 코드로 변환하는 함수
 const changeCharToCode = (char) => {
+  // 주어진 알파벳을 유니코드를 변환한다
+  // 실제로 이웃하는 알파벳인 A,와 B는 각각 65, 66이다.
   return char.charCodeAt();
 };
 
-// A의 아스키 코드, Z의 아스키 코드를 해시 테이블 형태로 저장
 const codeTable = {
+  // A의 아스키 코드, Z의 아스키 코드를 해시 테이블 형태로 저장한다
   A: changeCharToCode('A'),
   // 1을 더하는 이유는 A에서 Z로 바꾸기 위해 조이스틱을 조작하는 횟수
   Z: changeCharToCode('Z') + 1,
 };
 
-// 현재 알파벳을 아스키 코드로 변환하고,
-// 현재 알파벳이 A와 Z중에 어느 것에 가까운지 판단하기 위해서,
-// '현재 알파벳 아스키 코드 - A의 아스키 코드' 와 'Z의 아스키 코드 - 현재 알파벳 아스키 코드'중 작은 값을 반환한다.
-const countChangingChar = (char) => {
+const checkMinCodeFromChar = (char) => {
+  // 현재 알파벳을 아스키 코드로 변환하고, A부터 오름차순으로 찾는 것이 빠른지, Z부터 내림차순으로 찾는 것이 빠른지 판단하여 최소 움직임 횟수(?)를 리턴한다.
   const currCode = changeCharToCode(char);
   return Math.min(currCode - codeTable['A'], codeTable['Z'] - currCode);
 };
 
 const solution = (name) => {
-  console.log('A'.charCodeAt(), 'Z'.charCodeAt());
-  // 좌->우 이동하는 경우, 최소 이동 횟수
-  let minMoveCount = name.length - 1;
-  // 다음 문자열이 A인지 확인하기 위한 변수
+  // 최소이동 횟수, 초기값은 왼쪽에서 오른쪽으로만 이동하는 경우이다
+  let leastMoveCount = name.length - 1;
   let nextIdx = 0;
 
-  const totalCount = [...name].reduce((acc, char, idx) => {
+  const totalCountUpAndDown = [...name].reduce((acc, currChar, idx) => {
     nextIdx = idx + 1;
 
-    // 연속되는 A의 경우, 마지막 A의 다음 인덱스 구하기
+    // 현재 인덱스기준 다음 인덱스의 알파벳이 A인 경우 그 다음 인덱스로 넘어간다
     while (nextIdx < name.length && name.charAt(nextIdx) === 'A') {
       nextIdx++;
     }
 
-    // 2 * idx => 왔던 거리를 되돌아가는 횟수
-    // name.length - nextIdx => 다시 A를 만날때까지 이동하는 횟수
-    minMoveCount = Math.min(minMoveCount, 2 * idx + (name.length - nextIdx));
+    console.log('leastMoveCount: ', leastMoveCount, 'idx: ', idx, 'nextIdx: ', nextIdx);
 
-    return acc + countChangingChar(char);
+    // 즉, 연속되는 A가 있는 경우엔 마지막 A다음의 알파벳의 인덱스를 알아내서 (전체 길이 - 마지막 A다음의 알파벳의 인덱스)를 해야 거기까지 이동 횟수를 알 수 있다.
+
+    // 좌=>우로만 이동했을때의 횟수와 비교해서 더 작은 값을 할당한다
+    leastMoveCount = Math.min(leastMoveCount, 2 * idx + (name.length - nextIdx));
+
+    // 상하로 이동했을때의 횟수를 문자별로 누적시킨다
+    return acc + checkMinCodeFromChar(currChar);
   }, 0);
 
-  return totalCount + minMoveCount;
+  return totalCountUpAndDown + leastMoveCount;
 };
 
 test('run', () => {
-  expect(solution('JEROEN')).toBe(56);
-  expect(solution('JAN')).toBe(23);
+  // expect(solution('JEROEN')).toBe(56);
+  // expect(solution('JAN')).toBe(23);
+  expect(solution('BBBAABB')).toBe(23);
 });
