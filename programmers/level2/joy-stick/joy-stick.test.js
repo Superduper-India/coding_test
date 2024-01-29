4f192883670fff2949852ce59bfda96d0d6cb564
Superduper-India <sunyoung1542@gmail.com>
2024-01-29 23:48:49 +0900

    [level2] 조이스틱 다시 풀기

--- programmers/level2/joy-stick/joy-stick.test.js
+++ programmers/level2/joy-stick/joy-stick.test.js
@@ -7,54 +7,30 @@
 // ◀ - 커서를 왼쪽으로 이동 (첫 번째 위치에서 왼쪽으로 이동하면 마지막 문자에 커서)
 // ▶ - 커서를 오른쪽으로 이동 (마지막 위치에서 오른쪽으로 이동하면 첫 번째 문자에 커서)
 
-const changeCharToCode = (char) => {
-  // 주어진 알파벳을 유니코드를 변환한다
-  // 실제로 이웃하는 알파벳인 A,와 B는 각각 65, 66이다.
-  return char.charCodeAt();
-};
-
-const codeTable = {
-  // A의 아스키 코드, Z의 아스키 코드를 해시 테이블 형태로 저장한다
-  A: changeCharToCode('A'),
-  // 1을 더하는 이유는 A에서 Z로 바꾸기 위해 조이스틱을 조작하는 횟수
-  Z: changeCharToCode('Z') + 1,
-};
-
-const checkMinCodeFromChar = (char) => {
-  // 현재 알파벳을 아스키 코드로 변환하고, A부터 오름차순으로 찾는 것이 빠른지, Z부터 내림차순으로 찾는 것이 빠른지 판단하여 최소 움직임 횟수(?)를 리턴한다.
-  const currCode = changeCharToCode(char);
-  return Math.min(currCode - codeTable['A'], codeTable['Z'] - currCode);
+// 위아래로 이동시 A => Z 오름차순, Z => A 내림차순 어디가 더 가까운지 판단
+const charToCode = (targetChar) => {
+  const a = targetChar.charCodeAt() - 'A'.charCodeAt();
+  const b = 'Z'.charCodeAt() + 1 - targetChar.charCodeAt();
+  return Math.min(a, b);
 };
 
 const solution = (name) => {
-  // 최소이동 횟수, 초기값은 왼쪽에서 오른쪽으로만 이동하는 경우이다
-  let leastMoveCount = name.length - 1;
-  let nextIdx = 0;
+  let upAndDown = 0;
+  // 좌우로 이동시 최소 조작횟수
+  let leftAndRight = name.length - 1;
 
-  const totalCountUpAndDown = [...name].reduce((acc, currChar, idx) => {
-    nextIdx = idx + 1;
+  [...name].forEach((char, i) => {
+    upAndDown = upAndDown + charToCode(char);
 
-    // 현재 인덱스기준 다음 인덱스의 알파벳이 A인 경우 그 다음 인덱스로 넘어간다
-    while (nextIdx < name.length && name.charAt(nextIdx) === 'A') {
-      nextIdx++;
-    }
+    // 좌우로 이동시 A를 만나면 조이스틱을 조작하지 않는게 낫다.
+    // 현재 인덱스의 다음이 A면 돌아가야함
+  });
 
-    console.log('leastMoveCount: ', leastMoveCount, 'idx: ', idx, 'nextIdx: ', nextIdx);
-
-    // 즉, 연속되는 A가 있는 경우엔 마지막 A다음의 알파벳의 인덱스를 알아내서 (전체 길이 - 마지막 A다음의 알파벳의 인덱스)를 해야 거기까지 이동 횟수를 알 수 있다.
-
-    // 좌=>우로만 이동했을때의 횟수와 비교해서 더 작은 값을 할당한다
-    leastMoveCount = Math.min(leastMoveCount, 2 * idx + (name.length - nextIdx));
-
-    // 상하로 이동했을때의 횟수를 문자별로 누적시킨다
-    return acc + checkMinCodeFromChar(currChar);
-  }, 0);
-
-  return totalCountUpAndDown + leastMoveCount;
+  return upAndDown + leftAndRight;
 };
 
 test('run', () => {
   // expect(solution('JEROEN')).toBe(56);
   // expect(solution('JAN')).toBe(23);
-  expect(solution('BBBAABB')).toBe(23);
+  expect(solution('BBBAABB')).toBe(0);
 });
