0adb95c1a2047962b6085c7cca1e3aa5de2a8cfc
Superduper-India <sunyoung1542@gmail.com>
2024-05-07 14:39:24 +0900

    [level2] 조이스틱 다시 풀기

--- programmers/level2/joy-stick/joy-stick.test.js
+++ programmers/level2/joy-stick/joy-stick.test.js
@@ -7,36 +7,39 @@
 // ◀ - 커서를 왼쪽으로 이동 (첫 번째 위치에서 왼쪽으로 이동하면 마지막 문자에 커서)
 // ▶ - 커서를 오른쪽으로 이동 (마지막 위치에서 오른쪽으로 이동하면 첫 번째 문자에 커서)
 
-// 위아래로 이동시 A => Z 오름차순, Z => A 내림차순 어디가 더 가까운지 판단
-const charToCode = (targetChar) => {
-  const a = targetChar.charCodeAt() - 'A'.charCodeAt();
-  const b = 'Z'.charCodeAt() + 1 - targetChar.charCodeAt();
-  return Math.min(a, b);
-};
+const table = {
+  'A': 'A'.charCodeAt(),
+  'Z': 'Z'.charCodeAt() + 1 // Z에 조이스틱이 위치하게 하려면 1번 조작해야한다
+}
+
+const getMinCharCode = (targetCharCode) => {
+  const BottomUp = table['Z'] - targetCharCode // Z에서 현재 문자열의 코드를 뺀 숫자
+  const TopDown = targetCharCode - table['A'] // 현재 문자열의 코드에서 A를 뺀 숫자
+
+  return Math.min(BottomUp, TopDown)
+}
 
 const solution = (name) => {
-  let upAndDown = 0;
-  // 좌우로 이동시 최소 조작횟수
-  let leftAndRight = name.length - 1;
-  let nextIdx = 0;
+  let answer = 0
+  let currIdx = 0
+  // 좌우로 움직일때의 최솟값을 구해서 정답에 더한다
+  const leftToRight = name.length - 1
+  const rightToLeft = (name.length - 1) - name.lastIndexOf('A')
 
-  [...name].forEach((char, i) => {
-    upAndDown = upAndDown + charToCode(char);
-    nextIdx = i + 1;
+  while (currIdx <= name.length - 1) {
+    const currCharCode = name[currIdx].charCodeAt()
+    answer += getMinCharCode(currCharCode) // 상하로 움직일때의 최솟값을 구해서 정답에 더한다
+    currIdx++ // 다음 idx값
+  }
 
-    // 좌우로 이동시 다음 알파벳이 A를 만나면 돌아가는경우
-    if (name.charAt(nextIdx) === 'A') {
-      nextIdx++;
-    }
+  answer += Math.min(leftToRight, rightToLeft)
 
-    leftAndRight = Math.min(leftAndRight, i * 2 + (name.length - nextIdx));
-  });
-
-  return upAndDown + leftAndRight;
+  return answer
 };
 
 test('run', () => {
   expect(solution('JEROEN')).toBe(56);
   expect(solution('JAN')).toBe(23);
-  // expect(solution('BBBAABB')).toBe(0);
+  expect(solution('JAAN')).toBe(23);
+  expect(solution('JABN')).toBe(25);
 });
