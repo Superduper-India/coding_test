9bdb7a8b351b10cecb70914fd4300bca874744f1
Superduper-India <sunyoung1542@gmail.com>
2024-07-19 13:06:24 +0900

    [level2] 캐시 문제 다시 풀기

--- programmers/level2/cachee/cachee.test.js
+++ programmers/level2/cachee/cachee.test.js
@@ -10,26 +10,38 @@
 const solution = (cacheSize, cities) => {
   let answer = 0
   const cache = []
+  // 캐시 크기가 0인 경우 계속 cache miss이므로 따로 처리해줘야 한다.
+  if (cacheSize === 0) return cities.length * 5
 
   cities.forEach(city => {
     const targetCity = city.toLowerCase()
     if (!cache.includes(targetCity)) {
+      console.log('cache miss일 경우', cache, targetCity)
       // cache miss일 경우,
-      if (cache.length >= cacheSize) {
+      if (cache.length === cacheSize) {
         cache.shift() // 오래된 캐시를 지우기
+        console.log('오래된 캐시를 지움', cache)
       }
       cache.push(targetCity)
+      console.log('데이터를 새로 추가', cache)
       answer += 5
     } else {
+      // cache에서 targetCity를 제거해야함
+      cache.splice(cache.indexOf(targetCity), 1)
+      console.log('cache hit일 경우', cache, targetCity)
       // cache hit일 경우,
       answer += 1
     }
+    console.log('결과 answer값', answer)
   })
+
+  console.log(answer)
 
   return answer
 };
 
 test('run', () => {
+  expect(solution(0, ["a", "b", "a", "a", "b"])).toBe(25);
   expect(solution(3, ['Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA', 'Jeju', 'Pangyo', 'Seoul', 'NewYork', 'LA'])).toBe(50);
   expect(solution(3, ['Jeju', 'Pangyo', 'Seoul', 'Jeju', 'Pangyo', 'Seoul', 'Jeju', 'Pangyo', 'Seoul'])).toBe(21);
   expect(
