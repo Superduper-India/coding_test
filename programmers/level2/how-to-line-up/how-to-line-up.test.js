29e830e6d8d4175080d038a9f510f72cee1308af
Superduper-India <sunyoung1542@gmail.com>
2024-09-24 16:28:06 +0900

    [level2] 줄 서는 방법 문제 다시 풀기

--- programmers/level2/how-to-line-up/how-to-line-up.test.js
+++ programmers/level2/how-to-line-up/how-to-line-up.test.js
@@ -8,36 +8,36 @@
 // 경우의 수 중, 순열에 관련된 문제 같다.
 // 순열: 서로 다른 n개의 원소 중에서 r개를 중복 없이 골라 순서에 상관있게 나열하는 경우의 수 (nPr)
 // 순열의 공식: n! = n * (n-1) * (n-2) * ... * 1
-// [1,2,3]의 모든 순열을 구하려면 3! = 3 * 2 * 1 = 6가지
-
-const factorial = (n) => {
-  let res = 1;
-  for (let i = 2; i <= n; i++) res *= i;
-  return res;
-};
 
 // 2! = 2 * 1 = 2가지 => 이것이 앞자리 숫자의 교체 주기
-// arr[인덱스 / factorial(n-1)] 현재 인덱스 배열의 맨 앞자리 수
+
+const getFactorial = (number) => {
+  let result = 1;
+  // n! = n * (n-1) * (n-2) * ... * 1 를 리턴하는 함수
+  for (let i = 2; i <= number; i++) result = result * i;
+  return result;
+};
+
 const solution = (n, k) => {
   const answer = [];
+  const arr = [];
+  for (let i = 1; i <= n; i++) arr.push(i);
+  // n이 20이기만 해도 경우의 수가 2432902008176640000가지므로 모든 경우의 수를 찾는 것은 비효율 적이다.
+  let targetIndex = k - 1; // 실제로는 인덱스 위치이므로 초기화 해줌
 
-  const arr = new Array(n).fill(1);
-  for (let i = 1; i < n; i++) arr[i] = arr[i - 1] + 1;
-
-  let nth = k - 1; // 인덱스와 일치를 위해 k-1로 초기화한다.
-
-  // toDo 여기서부터 이해 안감.
   while (arr.length) {
-    if (nth === 0) {
+    // k가 1인 경우, 현재 배열을 그대로 리턴하면 된다.
+    if (targetIndex === 0) {
       answer.push(...arr);
       break;
     }
-    const fact = factorial(arr.length - 1); // 맨 앞자리수 교체 주기
-    const index = (nth / fact) >> 0; // nth번째의 맨 앞자리수 ???
-    nth = nth % fact;
+    // 주의!! arr.length는 반복문때 업데이트 되는데 n은 업데이트 안되므로, arr.length을 사용해야한다.
+    const factorial = getFactorial(n - 1); // 앞자리수 교체 주기 횟수
 
-    answer.push(arr[index]);
-    arr.splice(index, 1); // 배열에서 index요소 1개를 제거
+    const index = Math.floor(targetIndex / factorial); // k번째에 해당하는 arr의 인덱스를 찾기
+    targetIndex = targetIndex % factorial; // 다음에 찾아야할 k번째 인덱스 업데이트 (???)
+    answer.push(arr[index]); // arr의 k번째 인덱스 푸쉬
+    arr.splice(index, 1); // k번째 찾았으므로 요소 제거
   }
 
   return answer;
