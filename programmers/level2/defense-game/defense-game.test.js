91d2bc861cbcc20f38939c09dda8e0335200ad83
Superduper-India <sunyoung1542@gmail.com>
2024-09-27 16:16:23 +0900

    [level2] 디펜스 게임 문제 풀기

--- programmers/level2/defense-game/defense-game.test.js
+++ programmers/level2/defense-game/defense-game.test.js
@@ -6,18 +6,41 @@
 // - 무적권은 최대 k번 사용할 수 있다.
 
 const solution = (n, k, enemy) => {
-  // if (k === enemy.length) return k; // 무적권의 개수가 공격의 개수와 같으면 그대로 리턴
+  const answers = [];
+  if (k === enemy.length) return k; // 무적권의 개수가 공격의 개수와 같으면 그대로 리턴
 
   // n => 처음 가지고 있는 병사의 수
   // k => 사용 가능한 무적권의 횟수
   // enemy의 요소 => 매 라운드마다 공격해오는 적의 수
 
-  // k를 enemy의 요소로 나눠서 나머지의 경우의 수를 모두 구한다.
-  const rest = [];
+  const rest = []; // k를 enemy의 요소로 나눠서 나머지의 경우의 수를 모두 구한다.
   enemy.forEach((item) => {
     rest.push(n % item);
   });
-  console.log(rest);
+
+  let round = 0;
+
+  for (let i = 0; i < rest.length - 1; i++) {
+    const now = rest[i];
+    const next = rest[i + 1];
+    if (n < enemy[i] && !k) {
+      // console.log('현재: ', now, '다음: ', next, '현재 라운드: ', round, '남은 병사의 수: ', n, '남은 무적권의 수: ', k);
+      answers.push(round);
+      break;
+    } // 남은 병사의 수보다 현재 라운드의 적의 수가 더 많으면 게임 종료
+
+    if (now > next && k) {
+      // console.log('현재: ', now, '다음: ', next, '현재 라운드: ', round, '남은 병사의 수: ', n, '남은 무적권의 수: ', k);
+      k = k - 1; // 무적권 한 번 사용
+      round++; // 다음 라운드로,
+    } else {
+      // console.log('현재: ', now, '다음: ', next, '현재 라운드: ', round, '남은 병사의 수: ', n, '남은 무적권의 수: ', k);
+      n = n - enemy[i]; // 병사의 수가 줄어듬.
+      round++;
+    }
+  }
+
+  return answers[0];
 };
 
 test('run', () => {
