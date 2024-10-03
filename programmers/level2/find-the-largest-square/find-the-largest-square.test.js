7ee2653d82501251a63cfe46f51875f65e43828f
Superduper-India <sunyoung1542@gmail.com>
2024-10-03 16:07:59 +0900

    [level2] 가장 큰 정사각형 찾기 문제 다시 풀기

--- programmers/level2/find-the-largest-square/find-the-largest-square.test.js
+++ programmers/level2/find-the-largest-square/find-the-largest-square.test.js
@@ -6,30 +6,33 @@
 // 표의 값은 1또는 0으로만 이루어져 있다.
 // 표는 2차원 배열로 주어진다.
 
-const isSquare = (array) => {
-  // 좌표가 4개가 아니면 정사각형이 아니다.
-  if (array.length !== 4) return false;
-};
-
+// 만약 행 또는 열이 1이면 정사각형의 넓이는 1이다.
+// 루프를 돌며 자신의 위치(현재 인덱스)의 값이 1이상일 경우 왼쪽상단(↖︎), 위쪽(↑), 왼쪽(←)의 최솟값을 구한 후 자신의 위치에 최솟값 + 1을 할당한다.
+// 2번의 값의 최댓값을 answer에 할당해주고, 정사각형의 넓이를 구한다.(최댓값의 제곱 === 정사각형의 넓이)
 const solution = (board) => {
-  const points = [];
+  let answer = 0;
+  let row = board.length;
+  let col = board[0].length;
 
-  board.forEach((horizontial, x) => {
-    const temp = [];
-    horizontial.forEach((vertical, y) => {
-      // vertical이 1이면 x, y축 저장하기
-      if (vertical === 1) {
-        temp.push([x, y]);
-      } else temp.push([]);
-    });
-    points.push(temp);
-  });
+  // 행 또는 열이 1이면 정사각형의 넓이를 1으로 반환
+  if (row < 2 || col < 2) return 1;
 
-  isSquare(points);
+  for (let x = 1; x < row; x++) {
+    for (let y = 1; y < col; y++) {
+      // 자신의 위치(현재 인덱스)의 값이 1이상일 경우
+      if (board[x][y] !== 0) {
+        const topLeft = board[x - 1][y - 1];
+        const left = board[x][y - 1];
+        const top = board[x - 1][y];
+        const min = Math.min(topLeft, left, top); // 왼쪽상단, 위쪽, 왼쪽 중 최솟값
+        board[x][y] = min + 1;
+      }
 
-  // 정사각형은 모든 변의 길이가 같고, 두 대각선의 길이가 동일해야한다.
+      if (answer < board[x][y]) answer = board[x][y];
+    }
+  }
 
-  console.log('points:: ', points);
+  return answer * answer;
 };
 
 test('run', () => {
