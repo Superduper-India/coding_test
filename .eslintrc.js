a02b9a6effbaa39c2decd850a8909bd067728ab2
Superduper-India <sunyoung1542@gmail.com>
2024-11-07 18:34:33 +0900

    코테 린트 설정 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,22 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true,
-    "node": true,
-    "jest": true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended"
-  ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": "latest",
-    "sourceType": "module"
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  "plugins": [
-    "react"
-  ],
-  "rules": {
-    "semi": ["error", "always"],
-    "quotes": ["error", "double"],
-    "react/prop-types": "off"
-  }
+  plugins: ['react'],
+  rules: {
+    semi: ['warn', 'always'],
+    quotes: ['warn', 'single'],
+    'react/prop-types': 'off',
+  },
 };
