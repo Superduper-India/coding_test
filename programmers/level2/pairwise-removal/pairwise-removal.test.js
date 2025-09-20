4f9305bbeadcd4b3bf8aa3c6aed82d00b7a5db05
Superduper-India <sunyoung1542@gmail.com>
2025-09-20 14:22:11 +0900

    [level2] 짝지어 제거하기 문제 풀기

--- programmers/level2/pairwise-removal/pairwise-removal.test.js
+++ programmers/level2/pairwise-removal/pairwise-removal.test.js
@@ -1,24 +1,15 @@
 const solution = (s) => {
-  let stack = [];
-  for (let i = 0; i < s.length; i++) {
-    // 스택 배열이 빈배열이면, 요소 채워주기
-    if (!stack.length) stack.push(s[i]);
-    else {
-      if (stack[stack.length - 1] !== s[i]) {
-        stack.push(s[i]);
-        console.log("스택의 마지막 요소와 현재 요소가 일치하지 않는다.", stack);
-      } else {
-        // stack = []대신 stack.pop()을 사용하니까 테스트를 통과할 수 있었다
-        stack.pop();
-        console.log("스택의 마지막 요소와 현재 요소가 일치한다.", stack);
-      }
-    }
+  let count = 0;
+
+  for (let i = 0; i < s.length / 2; i++) {
+    // 다음요소랑 같으면(연속되면) 1, 그게 아니면 0
+    count = s[i - 1] === s[i] ? 1 : 0;
   }
 
-  return stack.length ? 0 : 1;
+  return count;
 };
 
-test("run", () => {
-  expect(solution("baabaa")).toBe(1);
-  expect(solution("cdcd")).toBe(0);
+test('run', () => {
+  expect(solution('baabaa')).toBe(1);
+  expect(solution('cdcd')).toBe(0);
 });
