e6a1a8989590178df4ea9ec7aeeeb61e71d45f97
Superduper-India <sunyoung1542@gmail.com>
2026-04-12 15:16:00 +0900

    [level2] 기능개발 문제 다시 풀기

--- programmers/level2/development-of-function/development-of-function.test.js
+++ programmers/level2/development-of-function/development-of-function.test.js
@@ -1,31 +1,24 @@
 function solution(progresses, speeds) {
-  let answer = [0];
-  const len = progresses.length;
-  const leftDays = new Array(len).fill(0);
+  const answer = [0];
 
-  for (let i = 0; i < len; i++) {
-    leftDays[i] = Math.ceil((100 - progresses[i]) / speeds[i]);
-  }
+  // 100에서 progresses[i]를 뺀 값을 speeds[i]로 나눈 값을 올림하여 남은 일수를 구한다.
+  const leftDays = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
 
-  let maxDay = leftDays[0];
+  let deployDay = leftDays[0];
 
-  for (let i = 0, j = 0; i < len; i++) {
-    if (leftDays[i] <= maxDay) { // 7이 7이랑 같거나 작으면, 
-      answer[j] += 1; // answer[0]이 1 증가
-    } else { // 9는 7보다 크다.
-      maxDay = leftDays[i]; // maxDay는 9로 갱신되고,
-      answer[++j] = 1; // answer[1]을 1부터 시작한다.
+  for (let i = 0, j = 0; i < leftDays.length; i++) {
+    if (deployDay >= leftDays[i]) {
+      answer[j] += 1;
+    } else {
+      deployDay = leftDays[i];
+      answer[++j] = 1;
     }
   }
 
   return answer;
 }
 
-test("run", () => {
-  expect(solution(
-    [93, 30, 55], [1, 30, 5]
-  )).toEqual([2, 1]);
-  expect(solution(
-    [95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]
-  )).toEqual([1, 3, 2]);
+test('run', () => {
+  expect(solution([93, 30, 55], [1, 30, 5])).toEqual([2, 1]);
+  expect(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1])).toEqual([1, 3, 2]);
 });
