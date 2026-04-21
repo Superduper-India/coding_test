5b86a94546fcf3ccb14ddce133ddce4012851034
Superduper-India <sunyoung1542@gmail.com>
2026-04-21 15:45:30 +0900

    [level2] 전화번호 목록 다시 풀기

--- programmers/level2/list-of-phone-numbers/list-of-phone-numbers.test.js
+++ programmers/level2/list-of-phone-numbers/list-of-phone-numbers.test.js
@@ -2,20 +2,14 @@
 // 전화번호부에 적힌 전화번호를 담은 배열 phone_book 이 solution 함수의 매개변수로 주어질 때, 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를 그렇지 않으면 true를 return 하도록 solution 함수를 작성
 
 const solution = (phone_book) => {
-  const hashList = {};
-
-  for (const number of phone_book[0]) {
-    if (!hashList[number]) hashList[number] = true;
-    else hashList[number] = false;
-  }
+  phone_book.sort();
 
   for (let i = 1; i < phone_book.length; i++) {
-    for (const number of phone_book[i]) {
-      if (hashList[number] === true) hashList[number] = false;
-    }
+    const temp = phone_book[i].slice(0, phone_book[0].length);
+    if (phone_book[0] === temp) return false;
   }
 
-  return !Object.values(hashList).every((it) => it === false);
+  return true;
 };
 
 test('run', () => {
