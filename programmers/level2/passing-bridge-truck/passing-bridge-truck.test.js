f6d25223baa80b716c6dca6efe5c45d193c7e180
Superduper-India <sunyoung1542@gmail.com>
2026-04-24 15:32:34 +0900

    [level2] 다리를 지나는 트럭 다시 풀기

--- programmers/level2/passing-bridge-truck/passing-bridge-truck.test.js
+++ programmers/level2/passing-bridge-truck/passing-bridge-truck.test.js
@@ -1,32 +1,33 @@
 function solution(bridge_length, weight, truck_weights) {
-  const passing_trucks = [];
-  const passed_trucks = [];
-  let second = 1;
-  let total_weight = 0;
+  const bridge = Array.from({ length: bridge_length }).fill(0);
+  let maxWeight = 0;
+  let count = 0;
 
-  let i = 0;
-  const length = truck_weights.length;
-  while (i < length) {
-    if (
-      passing_trucks.length <= bridge_length &&
-      total_weight <= weight
-    ) {
-      total_weight += truck_weights.shift();
-      passed_trucks.push(truck_weights.shift());
-      console.log(passed_trucks);
+  count++; // 시간 카운트
+  bridge.shift();
+  maxWeight += truck_weights[0]; // 하중 추가
+  bridge.push(truck_weights.shift()); // 다리 건너는 트럭추가
+
+  // 다리 위에 트럭이 남아있는 동안 반복문
+  while (maxWeight > 0) {
+    count++; // 시간 카운트
+
+    maxWeight -= bridge.shift();
+
+    // 가능하면 하중추가
+    if (maxWeight + truck_weights[0] <= weight) {
+      maxWeight += truck_weights[0]; // 하중 추가
+      bridge.push(truck_weights.shift()); // 다리 건너는 트럭추가
+    } else {
+      bridge.push(0);
     }
-    i++;
   }
+
+  return count;
 }
 
-test("run", () => {
-  expect(solution(
-    2, 10, [7, 4, 5, 6]
-  )).toEqual(8);
-  expect(solution(
-    100, 100, [10]
-  )).toEqual(101);
-  expect(solution(
-    100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
-  )).toEqual(110);
+test('run', () => {
+  expect(solution(2, 10, [7, 4, 5, 6])).toEqual(8);
+  expect(solution(100, 100, [10])).toEqual(101);
+  expect(solution(100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10])).toEqual(110);
 });
