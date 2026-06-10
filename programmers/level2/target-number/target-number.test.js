014b8b5bb2a2066ddaefe37e5efcb9dc09a9d107
Superduper-India <sunyoung1542@gmail.com>
2026-06-10 17:26:34 +0900

    [level2] 타겟넘버 다시 풀기

--- programmers/level2/target-number/target-number.test.js
+++ programmers/level2/target-number/target-number.test.js
@@ -1,22 +1,24 @@
+// 깊이 우선 탐색(순열)
+
 function solution(numbers, target) {
   let count = 0;
-  dfs(0, 0);
 
-  function dfs(index, sum) {
-    if (index === numbers.length) {
-      if (sum === target) {
-        count++;
-      } return;
+  function dfs(idx, sum) {
+    if (idx === numbers.length) {
+      if (sum === target) count++;
+      return;
     }
 
-    dfs(index + 1, sum + numbers[index]);
-    dfs(index + 1, sum - numbers[index]);
+    dfs(idx + 1, sum + numbers[idx]);
+    dfs(idx + 1, sum - numbers[idx]);
   }
+
+  dfs(0, 0);
 
   return count;
 }
 
-test("solution", () => {
+test('solution', () => {
   expect(solution([1, 1, 1, 1, 1], 3)).toBe(5);
   expect(solution([4, 1, 2, 1], 4)).toBe(2);
 });
